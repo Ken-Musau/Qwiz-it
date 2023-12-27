ce080dc6f9f9f7febbf414bfa0c8fb4380873931
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-27 18:00:55 +0300

    setup json dagta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log(state, action);
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const resp = await fetch("http://127.0.0.1:8000/questions");
+        const data = await resp.json();
+        console.log(data);
+      } catch (err) {
+        throw new Error("Broken link");
+      }
+    }
+
+    fetchQuestions();
+  }, []);
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
