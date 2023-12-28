39651c75634736a4a0a4fad16e8fabe3902b0a6d
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-28 11:28:14 +0300

    add and update component folder

--- src/StartScreen.js
+++
@@ -1,9 +0,0 @@
-function StartScreen() {
-  return (
-    <div>
-      <h2>Are you ready to answer questions?</h2>
-    </div>
-  );
-}
-
-export default StartScreen;
--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -17,14 +19,20 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("action unkown");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
-  console.log(questions);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
 
   useEffect(() => {
     async function fetchQuestions() {
@@ -51,7 +59,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
