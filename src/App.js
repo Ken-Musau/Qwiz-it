5d49ca8dbbbe8ab479a0695482338fe040d00a14
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-28 10:19:19 +0300

    add start screen page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -20,7 +23,8 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  console.log(questions);
 
   useEffect(() => {
     async function fetchQuestions() {
@@ -45,8 +49,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,9 @@
+function StartScreen() {
+  return (
+    <div>
+      <h2>Are you ready to answer questions?</h2>
+    </div>
+  );
+}
+
+export default StartScreen;
