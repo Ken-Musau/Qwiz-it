f2be5718fc233205e2df9b741240693ac7cb2bf9
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-15 20:28:49 +0300

    initialize usereducer in quizContext

--- src/context/quizContext.jsx
+++ src/context/quizContext.jsx
@@ -1,7 +1,25 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const QuizContext = createContext();
 
+const initialState = {};
+
+function reducer(state, action) {
+  switch (action.type) {
+  }
+}
+
 function QuizProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return <QuizContext.Provider>{children}</QuizContext.Provider>;
 }
+
+function useQuiz() {
+  const context = useContext(QuizContext);
+  if (context === undefined)
+    throw new Error("Quiz Context has been used outside the quiz provider");
+  return context;
+}
+
+export { QuizProvider, useQuiz };
